9ef0f57cb0684721f8bf18b25632a5d96ba91aef
Pedro Gozetto <pgozetto@gmail.com>
2024-12-04 18:02:27 -0300

    Update App.tsx

--- server/src/App.tsx
+++ server/src/App.tsx
@@ -2,13 +2,13 @@ import './App.css';
 import ShowImages from './components/ShowImages';
 
 function App() {
-              return (
-                    <div className="App">
-                    <header className="App-header">
-                            <ShowImages />
-                    </header>
-                    </div>
-                );
+        return (
+                <div className="App">
+                <header className="App-header">
+                        <ShowImages />
+                </header>
+                </div>
+        );
 }
 
 export default App;
